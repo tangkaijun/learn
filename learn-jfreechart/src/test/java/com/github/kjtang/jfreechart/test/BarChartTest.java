c7ab6eaf25035b7fc293a704e0f275afdc053796
kjtang <571856518@qq.com>
2018-03-25 21:23:33 +0800

    初始化项目

---
+++ learn-jfreechart/src/test/java/com/github/kjtang/jfreechart/test/BarChartTest.java
@@ -0,0 +1,15 @@
+package com.github.kjtang.jfreechart.test;
+
+import com.github.kjtang.jfreechart.BarChart;
+import org.junit.Test;
+
+/**
+ * Created by kaijun on 2018/3/25.
+ */
+public class BarChartTest {
+
+    @Test
+    public void testCreateChart() throws Exception{
+
+    }
+}
